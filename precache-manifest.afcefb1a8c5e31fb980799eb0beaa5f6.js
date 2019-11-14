4f732cbc912274fd0cb2d996082f0d3b4b682672
Alex Perez <alexperez@iMac-de-Alex.local>
2019-11-14 14:44:32 +0100

    Updates

---
+++ precache-manifest.afcefb1a8c5e31fb980799eb0beaa5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a259f866b42fbd8a63a306a110e753ac",
+    "url": "/reactmarvelapi/index.html"
+  },
+  {
+    "revision": "08f7ba7da14faa5258cc",
+    "url": "/reactmarvelapi/static/css/main.986397c3.chunk.css"
+  },
+  {
+    "revision": "9b6ae9d97f4208dd7f5a",
+    "url": "/reactmarvelapi/static/js/2.f35fc290.chunk.js"
+  },
+  {
+    "revision": "08f7ba7da14faa5258cc",
+    "url": "/reactmarvelapi/static/js/main.2401226e.chunk.js"
+  },
+  {
+    "revision": "518939bcc6bcc3b0efa0",
+    "url": "/reactmarvelapi/static/js/runtime-main.ead71ff7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactmarvelapi/static/media/logo.25bf045c.svg"
+  }
+]);
